95052db135bc7a527d2bc2a62663c5e65531e567
senpooja <poojasen744055@gmail.com>
2022-07-24 14:46:23 +0530

    uc-5-day10ass

---
+++ MultipuleContactUC5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        //Main  Method 
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Address Book Progam \n");
+            AddressBook createAddressBook = new AddressBook();
+            createAddressBook.ReadInput();
+            Console.ReadLine();
+
+        }
+    }
+}
