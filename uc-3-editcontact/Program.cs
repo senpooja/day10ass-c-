c88ad003cf7d7c2c5df1a27488358efd74df91e4
senpooja <poojasen744055@gmail.com>
2022-07-24 14:36:33 +0530

    uc-3-day10ass

---
+++ uc-3-editcontact/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+
+        //Main  Method / program Entry Point
+        static void Main(string[] args)
+        {
+            AddressBook createAddressBook = new AddressBook();
+            createAddressBook.ReadInput();
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
